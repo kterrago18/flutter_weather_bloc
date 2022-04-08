0c70094b07e9b29f989c4c254989c04cd001818b
Kenneth Terrago <kterrago091893@gmail.com>
2022-04-08 11:28:42 +0800

    feat: add model (de)serialization on location and weather models

--- packages/meta_weather_api/lib/src/models/location.dart
+++ packages/meta_weather_api/lib/src/models/location.dart
@@ -1,14 +1,40 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'location.g.dart';
+
+@JsonSerializable()
 class Location {
   final String title;
-  final String locationType;
+  final LocationType locationType;
   final int woeid;
+  @JsonKey(name: 'latt_long')
+  @LatLngConverter()
   final LatLong latLong;
+
   Location({
     required this.title,
     required this.locationType,
     required this.woeid,
     required this.latLong,
   });
+
+  factory Location.fromJson(Map<String, dynamic> json) =>
+      _$LocationFromJson(json);
+}
+
+enum LocationType {
+  @JsonValue('City')
+  city,
+  @JsonValue('Region')
+  region,
+  @JsonValue('State')
+  state,
+  @JsonValue('Province')
+  province,
+  @JsonValue('Country')
+  country,
+  @JsonValue('Continent')
+  continent
 }
 
 class LatLong {
@@ -18,4 +44,22 @@ class LatLong {
     required this.latitude,
     required this.longitude,
   });
+}
+
+class LatLngConverter implements JsonConverter<LatLong, String> {
+  const LatLngConverter();
+  @override
+  LatLong fromJson(String jsonString) {
+    final parts = jsonString.split(',');
+
+    return LatLong(
+      latitude: double.tryParse(parts[0]) ?? 0,
+      longitude: double.tryParse(parts[1]) ?? 0,
+    );
+  }
+
+  @override
+  String toJson(LatLong latLong) {
+    return '${latLong.latitude},${latLong.longitude}';
+  }
 }
--- packages/meta_weather_api/lib/src/models/weather.dart
+++ packages/meta_weather_api/lib/src/models/weather.dart
@@ -1,7 +1,14 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'weather.g.dart';
+
+@JsonSerializable()
 class Weather {
   final int id;
   final String weatherStateName;
+  @JsonKey(unknownEnumValue: WeatherState.unknown)
   final WeatherState weatherStateAbbr;
+  @JsonKey(unknownEnumValue: WindDirectionCompass.unknown)
   final WindDirectionCompass windDirectionCompass;
   final DateTime created;
   final DateTime applicableDate;
@@ -32,30 +39,55 @@ class Weather {
     required this.visibility,
     required this.predictability,
   });
+
+  factory Weather.fromJson(Map<String, dynamic> json) =>
+      _$WeatherFromJson(json);
 }
 
 enum WindDirectionCompass {
+  @JsonValue('N')
   north,
+  @JsonValue('NE')
   northEast,
+  @JsonValue('E')
   east,
+  @JsonValue('SE')
   southEast,
+  @JsonValue('S')
   south,
+  @JsonValue('SW')
   southWest,
+  @JsonValue('W')
   west,
+  @JsonValue('NW')
   northWest,
   unknown
 }
 
 enum WeatherState {
+  @JsonValue('sn')
   snow,
+  @JsonValue('sl')
   sleet,
+  @JsonValue('h')
   hail,
+  @JsonValue('t')
   thunderstorm,
+  @JsonValue('hr')
   heavyRain,
+  @JsonValue('lr')
   lightRain,
+  @JsonValue('s')
   showers,
+  @JsonValue('hc')
   heavyCloud,
+  @JsonValue('lc')
   lightCloud,
+  @JsonValue('c')
   clear,
   unknown
+}
+
+extension WeatherStateX on WeatherState {
+  String? get abbr => _$WeatherStateEnumMap[this];
 }
